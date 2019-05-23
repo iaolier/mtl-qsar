4d37fc4980c871feffaf3ec88c837228134824b1
Muireann Settle <muireannsettle4@gmail.com>
2019-05-23 11:08:14 +0100

    rmse function

---
+++ R/correlation function.R
@@ -0,0 +1,11 @@
+### r^2 correlation function
+
+ # <- function(truth, preds) {
+ #  correlation <- (sum((truth-mean(truth))*(preds-mean(preds))))/sqrt((sum((truth-mean(truth))^2)*sum((preds-mean(preds))^2)))
+ #  return(correlation)
+ #  }
+ #  
+  
+
+
+#correlation(x,y)
---
+++ tests/tst_rmse_r2.R
@@ -0,0 +1,10 @@
+
+library(devtools)
+load_all(".")
+
+x <- c(1,2,3,4)
+y <- c(1,4,5,2)
+
+rmse(x,y)
+
+
